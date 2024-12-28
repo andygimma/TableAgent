04a56e34e8279e636f5cd06924ee97ade9d7f6eb
Andy Gimma <andrewgimma@protonmail.com>
2024-12-28 13:21:09 -0500

    SignupForm

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -4,22 +4,24 @@ import supabaseClient from "../services/supabase";
 type Inputs = {
   email: string;
   password: string;
+  passwordConfirmation: string;
 };
 
-export default function LoginForm() {
+export default function SignupForm() {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    console.log(data);
-    await supabaseClient.auth.signInWithPassword({
-      password: data.password,
-      email: data.email,
+    await supabaseClient.auth.signUp({
+      email: data?.email,
+      password: data?.password,
     });
   };
 
+  const password123 = watch("password");
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" {...register("email", { required: true })} />
@@ -36,6 +38,20 @@ export default function LoginForm() {
       )}
       {errors?.password?.type === "minLength" && (
         <span>Password must be 8 characters long.</span>
+      )}
+      <input
+        type="password"
+        {...register("passwordConfirmation", {
+          required: "Confirm Password is required",
+          validate: (value) =>
+            value === password123 || "Passwords do not match",
+        })}
+      />
+      {errors?.passwordConfirmation?.type === "required" && (
+        <span>Password Confirmation is required.</span>
+      )}
+      {errors?.passwordConfirmation?.type === "validate" && (
+        <span>Passwords do not match.</span>
       )}
 
       <input type="submit" />
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,76 +1,9 @@
-import { useEffect, useState, useContext } from "react";
-import supabaseClient from "../services/supabase";
-import { useNavigate } from "react-router-dom";
-import AuthContext from "../contexts/Auth";
-import { ToastContainer, toast } from "react-toastify";
+import SignupForm from "../components/SignupForm";
 
 export default function Signup() {
-  const [data, setData] = useState({});
-  const navigate = useNavigate();
-
-  const loggedIn = useContext(AuthContext);
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-
-  useEffect(() => {
-    if (loggedIn) {
-      navigate("/");
-    }
-  }, [loggedIn]);
-
-  useEffect(() => {
-    const fetchCurrentUser = async () => {
-      const { data, error } = await supabaseClient.auth.getUser();
-      if (error) {
-      }
-      setData(data);
-    };
-    fetchCurrentUser();
-  }, []);
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    const { error } = await supabaseClient.auth.signUp({
-      email,
-      password,
-    });
-
-    if (error) {
-      toast(error.message, {
-        theme: "dark",
-      });
-      return;
-    }
-  };
   return (
     <div>
-      <ToastContainer />
-      <h1>Signup</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        <br />
-        <button type="submit">Signup</button>
-      </form>
+      <SignupForm />
     </div>
   );
 }
